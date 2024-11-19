62c820b8c73af0087dc20568334d9a8f71c7f75c
Mrityunjay Sharma <Sharmamrityunjay110@gmail.com>
2024-11-20 01:53:33 +0530

    committed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,4 @@
-
 global.foodData = require('./db')(function call(err, data, CatData) {
-  // console.log(data)
   if(err) console.log(err);
   global.foodData = data;
   global.foodCategory = CatData;
@@ -28,4 +26,3 @@ app.use('/api/auth', require('./Routes/Auth'));
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
-
